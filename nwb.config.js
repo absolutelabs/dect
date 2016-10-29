de3d022a5454de905f84701a860c734ba0990e5f
Arete Code <aretecode@gmail.com>
2016-10-29 02:52:30 -0700

    Add nwb.config.js

---
+++ nwb.config.js
@@ -0,0 +1,28 @@
+// https://github.com/insin/nwb/blob/master/docs/Configuration.md#webpack-configuration
+// http://stackoverflow.com/questions/32296967/webpack-dev-server-doesnt-generate-source-maps
+// https://webpack.github.io/docs/configuration.html#devtool
+// https://github.com/insin/nwb/blob/master/docs/Configuration.md#webpack-configuration
+// https://webpack.github.io/docs/configuration.html#output-devtoollinetoline
+module.exports = {
+  type: 'react-app',
+  babel: {
+    plugins: ['transform-async-to-generator', 'transform-flow-strip-types']
+  },
+  webpack: {
+    extra: {
+      devtool: '#source-map',
+      output: {
+        filename: 'index.js',
+        sourceMapFilename: '[file].map'
+      },
+      resolve: {
+        modulesDirectories: ['node_modules', 'src/npm_modules', 'src'],
+        alias: {
+          'reflux': 'core',
+          'react': 'preact-compat',
+          'react-dom': 'preact-compat',
+        }
+      }
+    }
+  }
+}
